6c0d6b1b7532827cec3c76010e42311ea86d787c
Iman Irandoost <imn.irdst@gmail.com>
2021-07-15 12:37:27 +0430

    Fix `is caching works` failing test for SearchVenuesLocalDataSource

--- app/src/main/java/com/imn/ii4sq/data/local/SearchVenuesMemoryCacheDao.kt
+++ app/src/main/java/com/imn/ii4sq/data/local/SearchVenuesMemoryCacheDao.kt
@@ -4,18 +4,24 @@ import com.imn.ii4sq.data.repository.SearchVenuesLocalDataSource
 import com.imn.ii4sq.domain.entities.Venue
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class SearchVenuesMemoryCacheDao(
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
-): SearchVenuesLocalDataSource {
+) : SearchVenuesLocalDataSource {
 
-    override suspend fun insert(lat: Double, lon: Double, radius: Double, venues: List<Venue>) {
-        TODO("Not yet implemented")
-    }
+    private val cache = mutableMapOf<String, List<Venue>>()
 
-    override suspend fun search(lat: Double, lon: Double, radius: Double): List<Venue>? {
-        TODO("Not yet implemented")
-    }
+    override suspend fun insert(lat: Double, lon: Double, radius: Double, venues: List<Venue>) =
+        withContext(ioDispatcher) {
+            cache[getKey(lat, lon, radius)] = venues
+        }
 
+    override suspend fun search(lat: Double, lon: Double, radius: Double): List<Venue>? =
+        withContext(ioDispatcher) {
+            cache[getKey(lat, lon, radius)]
+        }
 
+    private fun getKey(lat: Double, lon: Double, radius: Double) = "$lat-$lon-$radius"
 }
+
